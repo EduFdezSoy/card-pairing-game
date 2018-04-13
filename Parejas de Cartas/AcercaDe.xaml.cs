2975a568048690e524b142615e304d0323ba0c3e
Eduardo Fernandez <yo@edufdezsoy.es>
2018-04-13 20:28:17 +0200

    mas mierda

---
+++ Parejas de Cartas/AcercaDe.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Parejas_de_Cartas
+{
+    /// <summary>
+    /// Lógica de interacción para AcercaDe.xaml
+    /// </summary>
+    public partial class AcercaDe : Window
+    {
+        public AcercaDe()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
